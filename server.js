4e6731f49820be3e53fa275642f76103f87a412a
dabhimayank1 <ketankudecha@gmail.com>
2026-08-24 11:59:05 +0530

    Fix routing to always serve index.html

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const PUBLIC = path.join(__dirname);
 
-app.use(express.static(PUBLIC, {index:'index.html'}));
+app.use(express.static(__dirname));
 
-app.listen(PORT,()=>console.log(`Bandhan running at http://localhost:${PORT}`));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Bandhan running on port ${PORT}`);
+});
